c3f8452d50be1115900c13dc8410fcee701c65bf
dvgoyal <dvgoyalpce@gmail.com>
2022-03-28 17:10:09 +0530

    Add files via upload

---
+++ MyList.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+public class Program{
+    public static void Main(){
+        List<int> mylist = new List<int>();
+        for(int i=1; i<=10; i++)
+            mylist.Add(i);
+           
+        Console.WriteLine("using foreach loop");
+        foreach(int num in mylist)
+            Console.Write("{0} ", num);
+        Console.WriteLine(" ");
+          
+        Console.WriteLine("using index");
+        for(int i = 0; i< mylist.Count; i++)
+            Console.Write("{0} ", mylist[i]);
+        Console.WriteLine(" ");
+        
+        Console.WriteLine("removing 4 from list");
+        mylist.Remove(4); 
+        foreach(int num in mylist)
+            Console.Write("{0} ", num);
+        Console.WriteLine(" ");
+        
+        
+        Console.WriteLine("removing element at index 2 from list");
+        mylist.RemoveAt(2); 
+        foreach(int num in mylist)
+            Console.Write("{0} ", num);
+        Console.WriteLine(" ");
+    }
+}
